c135ff2e74a9cfe997a86875272471c90ea911ef
Joseph Lara <joseph.lara@gmail.com>
2025-01-23 12:51:41 +0200

    modify union file for fy25q1

--- Scripts/mer_genie_union.R
+++ Scripts/mer_genie_union.R
@@ -5,8 +5,8 @@
 
 library(tidyverse)
 library(lubridate)
-# library(gophr)
-# library(glamr)
+library(gophr)
+library(glamr)
 library(readxl)
 library(openxlsx)
 
@@ -17,15 +17,17 @@ library(openxlsx)
 # path_site_im_2018 <- "Data/genie/Genie_SITE_IM_2018.txt"
 # path_site_im_2019 <- "Data/genie/Genie_SITE_IM_2019.txt"
 # path_site_im_2020 <- "Data/genie/Genie_SITE_IM_2020.txt"
-# path_site_im_2021 <- "Data/genie/Genie_SITE_IM_2021.txt"
+path_site_im_2021 <- "Data/genie/Genie_SITE_IM_2021.txt"
 path_site_im_2022 <- "Data/genie/Genie_SITE_IM_2022.txt"
-path_site_im_2023_2024 <- "Data/genie/Genie_SITE_IM_2023_2024.txt"
+path_site_im_2023 <- "Data/genie/Genie_SITE_IM_2023.txt"
+path_site_im_2024_2025 <- "Data/genie/Genie_SITE_IM_2024_2025.txt"
 
 
 # path_dg_psnu_2019_2020 <- "Data/Genie/Genie_PSNU_IM_2019_2020.txt"
-# path_dg_psnu_2021 <- "Data/Genie/Genie_PSNU_IM_2021.txt"
+path_dg_psnu_2021 <- "Data/Genie/Genie_PSNU_IM_2021.txt"
 path_dg_psnu_2022 <- "Data/Genie/Genie_PSNU_IM_2022.txt"
-path_dg_psnu_2023_2024 <- "Data/Genie/Genie_PSNU_IM_2023_2024.txt"
+path_dg_psnu_2023 <- "Data/Genie/Genie_PSNU_IM_2023.txt"
+path_dg_psnu_2024_2025 <- "Data/Genie/Genie_PSNU_IM_2024_2025.txt"
 
 path_output_im_site <- "Data/genie/dg_ref.txt"
 path_output_im_site_rds <- "Data/genie/dg_ref.rds"
@@ -79,32 +81,33 @@ df <- read_delim(path, # OK
 # dg_2019 <- clean_genie(path_site_im_2019)
 # dg_2020 <- clean_genie(path_site_im_2020)
 # 
-# dg_2021 <- read_delim(path_site_im_2021, # OK
-#                       delim = "\t", escape_double = FALSE, 
-#                       trim_ws = TRUE) %>% 
-#   select(!c(country, 
-#             prime_partner_uei, 
-#             age_2018, 
-#             age_2019,
-#             indicatortype,
-#             source_name,
-#             mech_code,
-#             typemilitary,
-#             prime_partner_name,
-#             prime_partner_duns,
-#             award_number,
-#             community,
-#             communityuid,
-#             facilityuid,
-#             operatingunituid,
-#             facility,
-#             operatingunit,
-#             otherdisaggregate_sub,
-#             approvallevel,
-#             approvalleveldescription,
-#             dataelementuid,
-#             categoryoptioncombouid,
-#             snuprioritization))
+dg_2021 <- read_delim(path_site_im_2021, # OK
+                      delim = "\t", escape_double = FALSE,
+                      trim_ws = TRUE) %>%
+  select(!c(disaggregate,
+            country,
+            prime_partner_uei,
+            age_2018,
+            age_2019,
+            indicatortype,
+            source_name,
+            mech_code,
+            typemilitary,
+            prime_partner_name,
+            prime_partner_duns,
+            award_number,
+            community,
+            communityuid,
+            facilityuid,
+            operatingunituid,
+            facility,
+            operatingunit,
+            otherdisaggregate_sub,
+            approvallevel,
+            approvalleveldescription,
+            dataelementuid,
+            categoryoptioncombouid,
+            snuprioritization))
 
 dg_2022 <- read_delim(path_site_im_2022, # OK
                            delim = "\t", escape_double = FALSE, 
@@ -147,7 +150,49 @@ dg_2022 <- read_delim(path_site_im_2022, # OK
          psnuuid = snu2uid) 
 
 
-dg_2023_2024 <- read_delim(path_site_im_2023_2024, # OK
+dg_2023 <- read_delim(path_site_im_2023, # OK
+                           delim = "\t", escape_double = FALSE, 
+                           trim_ws = TRUE) %>% 
+  
+  select(!c(country, 
+            prime_partner_uei, 
+            age_2018, 
+            age_2019,
+            indicatortype,
+            source_name,
+            mech_code,
+            typemilitary,
+            prime_partner_name,
+            prime_partner_duns,
+            award_number,
+            community,
+            communityuid,
+            facilityuid,
+            operatingunituid,
+            facility,
+            operatingunit,
+            otherdisaggregate_sub,
+            approvallevel,
+            approvalleveldescription,
+            dataelementuid,
+            categoryoptioncombouid,
+            snuprioritization,
+            psnu,
+            psnuuid,
+            snu2,
+            snu2uid,
+            cop22_snuprioritization,
+            local_prime_partner,
+            use_for_age,
+            target_age_2024,
+            target_modality_2024,
+            safe_for_net_new,
+            safe_for_vlc)) %>% 
+  rename(psnu = cop22_psnu,
+         psnuuid = cop22_psnuuid)
+
+
+dg_2024_2025 <- read_delim(path_site_im_2024_2025, # OK
                       delim = "\t", escape_double = FALSE, 
                       trim_ws = TRUE) %>% 
   
@@ -188,13 +233,16 @@ dg_2023_2024 <- read_delim(path_site_im_2023_2024, # OK
   rename(psnu = cop22_psnu,
          psnuuid = cop22_psnuuid)
 
-# setdiff(names(dg_2022), names(dg_2023_2024))
-# setdiff(names(dg_2021), names(dg_2023_2024))
+setdiff(names(dg_2021), names(dg_2022))
+setdiff(names(dg_2022), names(dg_2023))
+setdiff(names(dg_2023), names(dg_2024_2025))
+setdiff(names(dg_2024_2025), names(dg_2023))
+setdiff(names(dg_2021), names(dg_2023_2024))
 
 # BIND DATASETS & CLEAN MECH NAMES -----------------------------------------------------------
 
 
-dg_2022_2024 <- bind_rows(dg_2022, dg_2023_2024) %>% 
+dg_2021_2025 <- bind_rows(dg_2021, dg_2022, dg_2023, dg_2024_2025) %>% 
   mutate(mech_name = recode(mech_name, 
                             
                             # clinical partner recoding
@@ -274,53 +322,53 @@ dg_2022_2024 <- bind_rows(dg_2022, dg_2023_2024) %>%
 
 
 # Clean Genie Issue That Links Ariel & Icap Cadeia Central To Military
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="siMZUtd2cJW" & dg_2022_2024$mech_name=="ARIEL"]<-"oX9C1fOEzSk"
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="oX9C1fOEzSk" & dg_2022_2024$mech_name=="ARIEL"]<-"Cadeia Central PS"
-dg_2022_2024$snu1[dg_2022_2024$orgunituid=="oX9C1fOEzSk" & dg_2022_2024$mech_name=="ARIEL"]<-"Maputo"
-dg_2022_2024$psnu[dg_2022_2024$orgunituid=="oX9C1fOEzSk" & dg_2022_2024$mech_name=="ARIEL"]<-"Matola"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="siMZUtd2cJW" & dg_2021_2025$mech_name=="ARIEL"]<-"oX9C1fOEzSk"
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="oX9C1fOEzSk" & dg_2021_2025$mech_name=="ARIEL"]<-"Cadeia Central PS"
+dg_2021_2025$snu1[dg_2021_2025$orgunituid=="oX9C1fOEzSk" & dg_2021_2025$mech_name=="ARIEL"]<-"Maputo"
+dg_2021_2025$psnu[dg_2021_2025$orgunituid=="oX9C1fOEzSk" & dg_2021_2025$mech_name=="ARIEL"]<-"Matola"
 
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="siMZUtd2cJW" & dg_2022_2024$mech_name=="ICAP"]<-"oX9C1fOEzSk"
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="oX9C1fOEzSk" & dg_2022_2024$mech_name=="ICAP"]<-"Cadeia Central PS"
-dg_2022_2024$snu1[dg_2022_2024$orgunituid=="oX9C1fOEzSk" & dg_2022_2024$mech_name=="ICAP"]<-"Maputo"
-dg_2022_2024$psnu[dg_2022_2024$orgunituid=="oX9C1fOEzSk" & dg_2022_2024$mech_name=="ICAP"]<-"Matola"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="siMZUtd2cJW" & dg_2021_2025$mech_name=="ICAP"]<-"oX9C1fOEzSk"
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="oX9C1fOEzSk" & dg_2021_2025$mech_name=="ICAP"]<-"Cadeia Central PS"
+dg_2021_2025$snu1[dg_2021_2025$orgunituid=="oX9C1fOEzSk" & dg_2021_2025$mech_name=="ICAP"]<-"Maputo"
+dg_2021_2025$psnu[dg_2021_2025$orgunituid=="oX9C1fOEzSk" & dg_2021_2025$mech_name=="ICAP"]<-"Matola"
 
 
 #Accounting for historical error in Chicavane and Chizavane: Converting EGPAF Chizavane to EGPAF Chicavane
-dg_2022_2024$psnu[dg_2022_2024$orgunituid=="EjFYleP5G9K" & dg_2022_2024$mech_name=="EGPAF"]<-"Chonguene"
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="EjFYleP5G9K" & dg_2022_2024$mech_name=="EGPAF"]<-"Chicavane CS"
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="EjFYleP5G9K" & dg_2022_2024$mech_name=="EGPAF"]<-"LqB6YZq9sG2"
+dg_2021_2025$psnu[dg_2021_2025$orgunituid=="EjFYleP5G9K" & dg_2021_2025$mech_name=="EGPAF"]<-"Chonguene"
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="EjFYleP5G9K" & dg_2021_2025$mech_name=="EGPAF"]<-"Chicavane CS"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="EjFYleP5G9K" & dg_2021_2025$mech_name=="EGPAF"]<-"LqB6YZq9sG2"
 
 
 #Accounting for historical error in Chicavane and Chizavane: Converting MISAU Chicavane to MISAU Chizavane
-dg_2022_2024$psnu[dg_2022_2024$orgunituid=="LqB6YZq9sG2" & dg_2022_2024$mech_name=="MISAU"]<-"Mandlakaze"
-dg_2022_2024$psnuuid[dg_2022_2024$orgunituid=="LqB6YZq9sG2" & dg_2022_2024$mech_name=="MISAU"]<-"VjJPal9jqUi"  # VERIFY THIS ADDRESSES ISSUE IDENTIFIED BY KIRSTIN
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="LqB6YZq9sG2" & dg_2022_2024$mech_name=="MISAU"]<-"Chizavane CS"
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="LqB6YZq9sG2" & dg_2022_2024$mech_name=="MISAU"]<-"EjFYleP5G9K"
+dg_2021_2025$psnu[dg_2021_2025$orgunituid=="LqB6YZq9sG2" & dg_2021_2025$mech_name=="MISAU"]<-"Mandlakaze"
+dg_2021_2025$psnuuid[dg_2021_2025$orgunituid=="LqB6YZq9sG2" & dg_2021_2025$mech_name=="MISAU"]<-"VjJPal9jqUi"  # VERIFY THIS ADDRESSES ISSUE IDENTIFIED BY KIRSTIN
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="LqB6YZq9sG2" & dg_2021_2025$mech_name=="MISAU"]<-"Chizavane CS"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="LqB6YZq9sG2" & dg_2021_2025$mech_name=="MISAU"]<-"EjFYleP5G9K"
 
 #Adjusting Sites Imported by PSM
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="FWbL33AHdCC" & dg_2022_2024$mech_name=="PSM"]<-"Mandlakazi HR"
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="FWbL33AHdCC" & dg_2022_2024$mech_name=="PSM"]<-"xlYle4v5GZ8"
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="FWbL33AHdCC" & dg_2021_2025$mech_name=="PSM"]<-"Mandlakazi HR"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="FWbL33AHdCC" & dg_2021_2025$mech_name=="PSM"]<-"xlYle4v5GZ8"
 
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="qM7gh9auEmt" & dg_2022_2024$mech_name=="PSM"]<-"Massinga CS"
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="qM7gh9auEmt" & dg_2022_2024$mech_name=="PSM"]<-"wSW662GVwZP"
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="qM7gh9auEmt" & dg_2021_2025$mech_name=="PSM"]<-"Massinga CS"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="qM7gh9auEmt" & dg_2021_2025$mech_name=="PSM"]<-"wSW662GVwZP"
 
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="W7CqwG3AuDh" & dg_2022_2024$mech_name=="PSM"]<-"Chicuque HR"
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="W7CqwG3AuDh" & dg_2022_2024$mech_name=="PSM"]<-"s28njgt1wYe"
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="W7CqwG3AuDh" & dg_2021_2025$mech_name=="PSM"]<-"Chicuque HR"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="W7CqwG3AuDh" & dg_2021_2025$mech_name=="PSM"]<-"s28njgt1wYe"
 
 
 #Setting up the HRSA/ ITECH at Alto Mae so it is not matched to CCS
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="jKg6rpNATKH" & dg_2022_2024$mech_name=="ITECH"]<-"jKg6rpNATKH_ITECH"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="jKg6rpNATKH" & dg_2021_2025$mech_name=="ITECH"]<-"jKg6rpNATKH_ITECH"
 
 # Adjusting Inhamudima to Inhamuzia to align with new COP22 reporting 
-dg_2022_2024$sitename[dg_2022_2024$orgunituid=="EslaBcDD5mZ"]<-"Inhamizua CS"
-dg_2022_2024$orgunituid[dg_2022_2024$orgunituid=="EslaBcDD5mZ"]<-"wmXdfKNX0qT"
+dg_2021_2025$sitename[dg_2021_2025$orgunituid=="EslaBcDD5mZ"]<-"Inhamizua CS"
+dg_2021_2025$orgunituid[dg_2021_2025$orgunituid=="EslaBcDD5mZ"]<-"wmXdfKNX0qT"
 
 
 # PRINT IM SITE OUTPUT TO DISK ----------------------------------------------------
 
 
 write_rds(
-  dg_2022_2024,
+  dg_2021_2025,
   path_output_im_site_rds)
 
 
@@ -386,7 +434,26 @@ dg_psnu_2022 <- read_delim(path_dg_psnu_2022) %>%
 
 
 
-dg_psnu_2023_2024 <- read_delim(path_dg_psnu_2023_2024) %>% 
+dg_psnu_2023 <- read_delim(path_dg_psnu_2023) %>% 
+  select(-c(operatingunit,
+            operatingunituid,
+            country,
+            snuprioritization,
+            typemilitary,
+            prime_partner_name,
+            mech_code,
+            prime_partner_duns,
+            award_number,
+            indicatortype,
+            categoryoptioncomboname,
+            age_2018,
+            age_2019,
+            otherdisaggregate_sub,
+            source_name,
+            cop22_psnu,
+            cop22_psnuuid))
+
+dg_psnu_2024_2025 <- read_delim(path_dg_psnu_2024_2025) %>% 
   select(-c(operatingunit,
             operatingunituid,
             country,
@@ -408,7 +475,7 @@ dg_psnu_2023_2024 <- read_delim(path_dg_psnu_2023_2024) %>%
 # setdiff(names(dg_psnu_2022), names(dg_psnu_2023_2024))
 # setdiff(names(dg_psnu_2023_2024), names(dg_psnu_2022))
 
-dg_psnu_2022_2024 <- bind_rows(dg_psnu_2022, dg_psnu_2023_2024) %>%
+dg_psnu_2022_2025 <- bind_rows(dg_psnu_2022, dg_psnu_2023, dg_psnu_2024_2025) %>%
   mutate(mech_name = recode(mech_name, 
                             
                             # clinical partner recoding
@@ -488,6 +555,6 @@ dg_psnu_2022_2024 <- bind_rows(dg_psnu_2022, dg_psnu_2023_2024) %>%
 
 
 write_rds(
-  dg_psnu_2022_2024,
+  dg_psnu_2022_2025,
   path_output_im_psnu_rds)
 
